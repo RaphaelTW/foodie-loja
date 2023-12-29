ed97064b1f11fd2794f76e29e736bba9cecd6635
Raphael Laurentino da Silva <77465404+RaphaelTW@users.noreply.github.com>
2023-12-29 12:40:22 -0300

    Refaturamento no FOOTER, Navbar e DaekMode

--- src/components/Navbar/DarkMode.jsx
+++ src/components/Navbar/DarkMode.jsx
@@ -3,45 +3,36 @@ import darkPng from "../../assets/website/dark-mode-button.png";
 import lightPng from "../../assets/website/light-mode-button.png";
 
 const DarkMode = () => {
-  const [theme, setTheme] = useState(
-    localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
-  );
-  const element = document.documentElement;
+    const storedTheme = localStorage.getItem("theme") || "light";
+    const [theme, setTheme] = useState(storedTheme);
+    const element = document.documentElement;
 
-  useEffect(() => {
-    if (theme === "dark") {
-      element.classList.add("dark");
-      localStorage.setItem("theme", "dark");
-    } else {
-      element.classList.remove("dark");
-      localStorage.setItem("theme", "light");
-    }
-  }, [theme]);
-  return (
-    <>
-      <div className="relative ">
-        <img
-          // src={theme === "dark" ? darkPng : lightPng}
-          src={lightPng}
-          alt="dark"
-          onClick={() =>
-            setTheme((data) => (data === "dark" ? "light" : "dark"))
-          }
-          className={`w-12 cursor-pointer drop-shadow-[1px_1px_1px_rgba(0,0,0,0.1)] transition-all duration-300 absolute right-0 z-10  ${
-            theme === "dark" ? "opacity-0" : "opacity-100"
-          } `}
-        />
-        <img
-          src={darkPng}
-          alt="dark"
-          onClick={() =>
-            setTheme((data) => (data === "dark" ? "light" : "dark"))
-          }
-          className="w-12 cursor-pointer drop-shadow-[1px_1px_2px_rgba(0,0,0,0.5)] duration-300 "
-        />
-      </div>
-    </>
-  );
+    useEffect(() => {
+        element.classList.toggle("dark", theme === "dark");
+        localStorage.setItem("theme", theme);
+    }, [theme]);
+
+    const toggleTheme = () => {
+        setTheme((prevTheme) => (prevTheme === "dark" ? "light" : "dark"));
+    };
+
+    return (
+        <div className="relative">
+            <img
+                src={theme === "dark" ? darkPng : lightPng}
+                alt="dark"
+                onClick={toggleTheme}
+                className={`w-12 cursor-pointer drop-shadow-[1px_1px_1px_rgba(0,0,0,0.1)] transition-all duration-300 absolute right-0 z-10 ${theme === "dark" ? "opacity-0" : "opacity-100"
+                    }`}
+            />
+            <img
+                src={darkPng}
+                alt="dark"
+                onClick={toggleTheme}
+                className="w-12 cursor-pointer drop-shadow-[1px_1px_2px_rgba(0,0,0,0.5)] duration-300"
+            />
+        </div>
+    );
 };
 
 export default DarkMode;
